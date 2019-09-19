c8011102ffef919778967b4f7f665e2623988c3b
blackwellsmith <blackwellsmithiv@gmail.com>
2019-09-19 11:58:08 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors
-  resources :patients
   resources :appointments, only: [:show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doctors, only: [:show, :index]
+  resources :patients, only: [:show, :index]
+  #resources :appointments, only: [:show]
 end
